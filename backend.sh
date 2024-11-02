84a42972b6ac0e3df8d4a84c8fa8924adfac77d1
SKY1GER <saikumarb9119@gmail.com>
2024-11-02 21:12:34 +0530

    Complete expense app automatic configuration

--- backend.sh
+++ backend.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-echo "please enter mysql password"
-read -s my_sql_password
+
 
 source ./common.sh
 
 check_root
 
+echo "please enter mysql password"
+read -s my_sql_password
+
 dnf module disable nodejs -y &>>$logfile
-validate $? "$G sccessfully disabled $N"
+VALIDATE $? "$G sccessfully disabled $N"
 
 dnf module enable nodejs:20 -y &>>$logfile
-validate $? "$G enabled nodejs:20 $N"
+VALIDATE $? "$G enabled nodejs:20 $N"
 
 dnf install nodejs -y &>>$logfile
-validate $? "$G installed nodejs:20 $N"
+VALIDATE $? "$G installed nodejs:20 $N"
 
 id expense &>>$logfile
 if [ $? -ne 0 ]
 then
     useradd expense &>>$logfile
-    validate $? "$G user expense added $N"
+    VALIDATE $? "$G user expense added $N"
 else
     echo -e "$G ***userexpense already present*** $Y ***Skipping*** $N"
 fi
@@ -28,39 +30,39 @@ fi
 mkdir -p /app &>>$logfile
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$logfile
-validate $? "$Y ***downling backend code*** $N"
+VALIDATE $? "$Y ***downling backend code*** $N"
 
 
 cd /app &>>$logfile
 rm -rf /app/*
 unzip /tmp/backend.zip &>>$logfile
-validate $? "$Y ***unzipping backend file*** $N"
+VALIDATE $? "$Y ***unzipping backend file*** $N"
 
 cd /app &>>$logfile
 npm install &>>$logfile
-validate $? "$G ***installing npm*** $N"
+VALIDATE $? "$G ***installing npm*** $N"
 
 cp /home/ec2-user/expense-shell/backend.service  /etc/systemd/system/backend.service &>>$logfile
-validate $? "copied backend service"
+VALIDATE $? "copied backend service"
 
 systemctl daemon-reload &>>$logfile
-validate $? "daemon-reload"
+VALIDATE $? "daemon-reload"
 
 systemctl start backend &>>$logfile
-validate $? "start backend"
+VALIDATE $? "start backend"
 
 systemctl enable backend &>>$logfile
-validate $? "enable backend"
+VALIDATE $? "enable backend"
 
 
 dnf install mysql-server -y &>>$logfile
-validate $? "mysql-server"
+VALIDATE $? "mysql-server"
 
 mysql -h db.daws79s.online -uroot -p${my_sql_password} < /app/schema/backend.sql &>>$logfile
-validate $? "loading schema"
+VALIDATE $? "loading schema"
 #sudo cat /app/schema/backend.sql
 
 systemctl restart backend &>>$logfile
-validate $? "restating backend"
+VALIDATE $? "restating backend"
 
 #systemctl status backend
--- frontend.sh
+++ frontend.sh
@@ -6,25 +6,25 @@ source ./common.sh
 check_root
 
 dnf install nginx -y 
-validate $? "installing nginx"
+VALIDATE $? "installing nginx"
 
 systemctl enable nginx
-validate $? "enabling nginx"
+VALIDATE $? "enabling nginx"
 
 systemctl start nginx
-validate $? "starting nginx server"
+VALIDATE $? "starting nginx server"
 
 rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
-validate $? "Download the frontend content"
+VALIDATE $? "Download the frontend content"
 
 cd /usr/share/nginx/html
 
 unzip /tmp/frontend.zip
-validate $? "Extract the frontend content"
+VALIDATE $? "Extract the frontend content"
 
 cp /home/ec2-user/expense-shell/expense.conf  /etc/nginx/default.d/expense.conf &>>$logfile
 
 systemctl restart nginx
-validate $? "restaring the nginx"
+VALIDATE $? "restaring the nginx"
